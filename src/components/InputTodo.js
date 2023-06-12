4dd9bdf98416f2e69353c1dc17aeb0830887be69
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-12 16:40:29 +0300

    Add input todo list handler

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -8,6 +8,16 @@ const InputTodo = (props) => {
         value: e.target.value
       });
     };
-
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (title.value.trim()) {
+          props.addTodoProps(title.value);
+          setTitle({
+            value: "",
+          });
+        } else {
+          alert("Please write item");
+        }
+      };
     
 }
