5798f6090220e149fdc406beedb3fa4b83d7f7b8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-12 16:13:29 +0300

    Add header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,4 +5,21 @@ const Header=()=>{
         padding: "20px 0",
         lineHeight: "1.5em",
     }
-}
+    return (
+        <header style={headerStyle}>
+          <h1
+            style={{
+              fontSize: "6rem",
+              fontWeight: "600",
+              marginBottom: "2rem",
+              lineHeight: "1em",
+              color: "#ececec",
+              textTransform: "lowercase",
+              textAlign: "center",
+            }}
+          >
+            todos
+          </h1>
+        </header>
+      )
+    }
